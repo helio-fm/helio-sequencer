bdabe15d54b3e23f3baf381993cca3faec465e3c
Peter Rudenko <peter.rudenko@gmail.com>
2017-05-20 07:55:03 +0400

    Update projects and configs to plug yet another CI service

--- .travis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Travis builds on Trusty image, and webkit2gtk-4.0 dev package is not available.
-# But Projucer adds this dependency in the makefile,
-# even if the project doesn't need it, i.e. has JUCE_WEB_BROWSER=0.
-# So here is the dirty hackaround:
-cd Projects/LinuxMakefile
-sed -i 's/webkit2gtk-4.0//g' Makefile
-make
