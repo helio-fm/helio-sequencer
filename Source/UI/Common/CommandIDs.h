9edfc455fbda77fa89ce1b37395017db0fbb6467
Peter Rudenko <peter.rudenko@gmail.com>
2017-10-08 17:44:36 +0400

    Implement basic hotkey scheme class and update command id's

--- Source/UI/Common/CommandIDs.h
+++ Source/UI/Common/CommandIDs.h
@@ -19,161 +19,170 @@
 
 namespace CommandIDs
 {
-    enum
+    enum UI
     {
-        Back                            = 0x000001,
-        Cancel                          = 0x000002,
-        Browse                          = 0x000003,
-        IconButtonPressed               = 0x000004,
-        MenuButtonPressed               = 0x000005,
-        RootTreeItemPressed             = 0x000006,
-        HideRollover                    = 0x000007,
-        HideDialog                      = 0x000008,
-        HideCallout                     = 0x000009,
-        
+        Back                            = 0x0001,
+        Cancel                          = 0x0002,
+        Browse                          = 0x0003,
+        IconButtonPressed               = 0x0004,
+        MenuButtonPressed               = 0x0005,
+        RootTreeItemPressed             = 0x0006,
+        HideRollover                    = 0x0007,
+        HideDialog                      = 0x0008,
+        HideCallout                     = 0x0009,
+
         // TimeSignatureCommandPanel
-        ChangeTimeSignature             = 0x000100,
-        ChangeTimeSignatureConfirmed    = 0x000101,
-        DeleteTimeSignature             = 0x000102,
+        ChangeTimeSignature             = 0x0010,
+        ChangeTimeSignatureConfirmed    = 0x0011,
+        DeleteTimeSignature             = 0x0012,
 
         // AnnotationCommandPanel
-        RenameAnnotation                = 0x000110,
-        RenameAnnotationConfirmed       = 0x000111,
-        SetAnnotationColour             = 0x000112, // more ids reserved for colours
-        DeleteAnnotation                = 0x001000,
-        
+        RenameAnnotation                = 0x0020,
+        RenameAnnotationConfirmed       = 0x0021,
+        SetAnnotationColour             = 0x0022, // more ids reserved for colours
+        DeleteAnnotation                = 0x0100,
+
         // AnnotationsCommandPanel
-        AddAnnotation                   = 0x001001,
-        AddAnnotationConfirmed          = 0x001002,
-        AddTimeSignature                = 0x001003,
-        AddTimeSignatureConfirmed       = 0x001004,
-        JumpToAnnotation                = 0x001005, // more ids reserved for annotations
+        AddAnnotation                   = 0x0101,
+        AddAnnotationConfirmed          = 0x0102,
+        AddTimeSignature                = 0x0103,
+        AddTimeSignatureConfirmed       = 0x0104,
+        JumpToAnnotation                = 0x0105, // more ids reserved for annotations
 
         // ArpeggiatorEditorPanel
-        ResetArpeggiatorChanges         = 0x002000,
-        ApplyArpeggiator                = 0x002001, // more ids reserved for arps
-        
-        InitWorkspace                   = 0x003001,
-        
-        RenameInstrument                = 0x003002,
-        UpdateInstrument                = 0x003003,
-        DeleteInstrument                = 0x003004,
+        ResetArpeggiatorChanges         = 0x0200,
+        ApplyArpeggiator                = 0x0201, // more ids reserved for arps
 
-        ScanAllPlugins                  = 0x003005,
-        ScanPluginsFolder               = 0x003006,
-        CreateInstrument                = 0x003007, // more ids reserved for instruments
-        
+        InitWorkspace                   = 0x0500,
+
+        RenameInstrument                = 0x0501,
+        UpdateInstrument                = 0x0502,
+        DeleteInstrument                = 0x0503,
+
+        ScanAllPlugins                  = 0x0504,
+        ScanPluginsFolder               = 0x0505,
+        CreateInstrument                = 0x0506, // more ids reserved for instruments
+
         // LayerCommandPanel
-        DeleteLayer                     = 0x004000,
-        MuteLayer                       = 0x004001,
-        UnmuteLayer                     = 0x004002,
-        RenameLayer                     = 0x004003,
-        RenameLayerConfirmed            = 0x004004,
-        SelectLayerColour               = 0x004005,
-        SelectLayerInstrument           = 0x004006,
-        SelectAllEvents                 = 0x004007,
-        DuplicateLayerTo                = 0x004008,
-        
-        SetLayerColour                  = 0x004009, // more ids reserved for layers
-        SetLayerInstrument              = 0x005000, // more ids reserved for instruments
-        MoveLayerToProject              = 0x006000, // more ids reserved for projects
-        
+        DeleteLayer                     = 0x1000,
+        MuteLayer                       = 0x1001,
+        UnmuteLayer                     = 0x1002,
+        RenameLayer                     = 0x1003,
+        RenameLayerConfirmed            = 0x1004,
+        SelectLayerColour               = 0x1005,
+        SelectLayerInstrument           = 0x1006,
+        SelectAllEvents                 = 0x1007,
+        DuplicateLayerTo                = 0x1008,
+
+        SetLayerColour                  = 0x1009, // more ids reserved for layers
+        SetLayerInstrument              = 0x1100, // more ids reserved for instruments
+        MoveLayerToProject              = 0x1500, // more ids reserved for projects
+
         // MidiRollCommandPanel
-        EditEvents                      = 0x007000,
-        DeleteEvents                    = 0x007001,
-        
-        CopyEvents                      = 0x007002,
-        CutEvents                       = 0x007003,
-        PasteEvents                     = 0x007004,
-        MoveEventsToLayer               = 0x007005, // more ids reserved for layers
-        
-        CursorTool                      = 0x008000,
-        DrawTool                        = 0x008001,
-        SelectionTool                   = 0x008002,
-        ZoomTool                        = 0x008003,
-        DragTool                        = 0x008004,
-        InsertSpaceTool                 = 0x008005,
-        WipeSpaceTool                   = 0x008006,
-        ScissorsTool                    = 0x008007,
-        
-        ZoomIn                          = 0x008008,
-        ZoomOut                         = 0x008009,
-        
-        Undo                            = 0x008010,
-        Redo                            = 0x008011,
-        
-        DeleteNotes                     = 0x008012,
-        DuplicateNotes                  = 0x008013,
-        ArpNotes                        = 0x008014,
-        TweakNotesVolume                = 0x008015,
-        
-        ShowAnnotations                 = 0x008016,
+        DeleteEvents                    = 0x1601,
+        CopyEvents                      = 0x1602,
+        CutEvents                       = 0x1603,
+        PasteEvents                     = 0x1604,
 
-        ResetVolumeChanges              = 0x009000,
-        
-        ApplyOpenGLRenderer             = 0x010000,
-        
-        TransportStartPlayback          = 0x011000,
-        TransportPausePlayback          = 0x011001,
+        MoveEventsToLayer               = 0x1605, // more ids reserved for layers
 
-        PopupMenuDismiss                = 0x012000,
-        PopupMenuDismissedAsDone        = 0x012001,
-        PopupMenuDismissedAsCancel      = 0x012002,
+        CursorTool                      = 0x2000,
+        DrawTool                        = 0x2001,
+        SelectionTool                   = 0x2002,
+        ZoomTool                        = 0x2003,
+        DragTool                        = 0x2004,
+        InsertSpaceTool                 = 0x2005,
+        WipeSpaceTool                   = 0x2006,
+        ScissorsTool                    = 0x2007,
+
+        ZoomIn                          = 0x2008,
+        ZoomOut                         = 0x2009,
+
+        Undo                            = 0x200a,
+        Redo                            = 0x200b,
+
+        DeleteNotes                     = 0x200c,
+        DuplicateNotes                  = 0x200d,
+        ArpNotes                        = 0x200e,
+        TweakNotesVolume                = 0x200f,
+
+        ShowAnnotations                 = 0x2010,
+
+        ResetVolumeChanges              = 0x2011,
+
+        ApplyOpenGLRenderer             = 0x2012,
+
+        TransportStartPlayback          = 0x2013,
+        TransportPausePlayback          = 0x2014,
+
+        PopupMenuDismiss                = 0x2015,
+        PopupMenuDismissedAsDone        = 0x2016,
+        PopupMenuDismissedAsCancel      = 0x2017,
 
         // TreePanel
-        SelectRootItemPanel             = 0x013000,
-        DeselectRootItemPanel           = 0x013001,
-        UpdateRootItemPanel             = 0x013002,
-        
+        SelectRootItemPanel             = 0x2018,
+        DeselectRootItemPanel           = 0x2019,
+        UpdateRootItemPanel             = 0x201a,
+
         // Version control
-        VersionControlForcePull         = 0x014000,
-        VersionControlReset             = 0x014001,
-        VersionControlAmend             = 0x014002,
-        VersionControlCommit            = 0x014003,
-        
+        VersionControlForcePull         = 0x201b,
+        VersionControlReset             = 0x201c,
+        VersionControlAmend             = 0x201d,
+        VersionControlCommit            = 0x201e,
+
         // WorkspaceMenu
-        LoginLogout                     = 0x015000,
-        OpenProject                     = 0x015001,
-        CreateNewProject                = 0x015002,
-        
+        LoginLogout                     = 0x2020,
+        OpenProject                     = 0x2021,
+        CreateNewProject                = 0x2022,
+
         // ProjectCommandPanel
-        RenderToFLAC                    = 0x016000,
-        RenderToOGG                     = 0x016001,
-        RenderToWAV                     = 0x016002,
-        
-        AddLayer                        = 0x016003,
-        AddLayerConfirmed               = 0x016004,
-        AddAutomation                   = 0x016005,
-        
-        ImportMidi                      = 0x016006,
-        ExportMidi                      = 0x016007,
-        
-        UnloadProject                   = 0x016008,
-        DeleteProject                   = 0x016009,
-        DeleteProjectConfirmed1         = 0x016010,
-        DeleteProjectConfirmed2         = 0x016011,
-        
-        RefactorTransposeUp             = 0x016012,
-        RefactorTransposeDown           = 0x016013,
-        RefactorRemoveOverlaps          = 0x016014,
-        
-        ProjectMainMenu                 = 0x016015,
-        ProjectRenderMenu               = 0x016016,
-        ProjectBatchMenu                = 0x016017,
-        ProjectAutomationsMenu          = 0x016018,
-        ProjectInstrumentsMenu          = 0x016019, // more ids reserved for instruments
-        
-        AddTempoController              = 0x017000,
-        AddCustomController             = 0x017001, // more ids reserved for controllers
-        
-        BatchChangeInstrument           = 0x018000,
-        BatchSetInstrument              = 0x018001, // more ids reserved for instruments
+        RenderToFLAC                    = 0x2030,
+        RenderToOGG                     = 0x2031,
+        RenderToWAV                     = 0x2032,
 
-        DismissModalDialogAsync         = 0x020000,
+        AddLayer                        = 0x2040,
+        AddLayerConfirmed               = 0x2041,
+        AddAutomation                   = 0x2042,
 
-        SelectFunction                  = 0x020010,
-        SelectScale                     = 0x020100, // more ids reserved for sub-items
+        ImportMidi                      = 0x2050,
+        ExportMidi                      = 0x2051,
 
-        YourNextCommandId               = 0x021000
+        UnloadProject                   = 0x2060,
+        DeleteProject                   = 0x2061,
+        DeleteProjectConfirmed1         = 0x2062,
+        DeleteProjectConfirmed2         = 0x2063,
+
+        RefactorTransposeUp             = 0x2070,
+        RefactorTransposeDown           = 0x2071,
+        RefactorRemoveOverlaps          = 0x2072,
+
+        ProjectMainMenu                 = 0x2080,
+        ProjectRenderMenu               = 0x2081,
+        ProjectBatchMenu                = 0x2082,
+        ProjectAutomationsMenu          = 0x2083,
+        ProjectInstrumentsMenu          = 0x2084, // more ids reserved for instruments
+
+        AddTempoController              = 0x2500,
+        AddCustomController             = 0x2501, // more ids reserved for controllers
+
+        BatchChangeInstrument           = 0x2600,
+        BatchSetInstrument              = 0x2601, // more ids reserved for instruments
+
+        DismissModalDialogAsync         = 0x3000,
+
+        SelectFunction                  = 0x3010,
+        SelectScale                     = 0x3011, // more ids reserved for sub-items
+
+        SwitchBetweenRolls              = 0x3200,
+        ShowPreviousPage                = 0x3201,
+        ShowNextPage                    = 0x3202,
+        ToggleShowHideConsole           = 0x3203,
+
+        StartDragViewport               = 0x3204,
+        EndDragViewport                 = 0x3205,
+        YourNextCommandId = 0x3206
     };
+
+    int getIdForName(const String &command);
+
 } // namespace CommandIDs
---
+++ Source/UI/Input/HotkeyScheme.h
@@ -0,0 +1,67 @@
+/*
+    This file is part of Helio Workstation.
+
+    Helio is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Helio is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Helio. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "Serializable.h"
+
+class HotkeyScheme final : public Serializable
+{
+public:
+
+    HotkeyScheme();
+
+    class Hotkey final
+    {
+    public:
+        KeyPress keyPress;
+        String componentId;
+        int commandId;
+    };
+
+    bool dispatchKeyPress(KeyPress keyPress,
+        WeakReference<Component> keyPressReceiver,
+        WeakReference<Component> messageReceiverParent);
+
+    bool dispatchKeyStateChange(bool isKeyDown,
+        WeakReference<Component> keyPressReceiver,
+        WeakReference<Component> messageReceiverParent);
+
+    //===------------------------------------------------------------------===//
+    // Serializable
+    //===------------------------------------------------------------------===//
+
+    XmlElement *serialize() const override;
+    void deserialize(const XmlElement &xml) override;
+    void reset() override;
+
+private:
+
+    String name;
+    Array<Hotkey> keyPresses;
+    Array<Hotkey> keyDowns;
+    Array<Hotkey> keyUps;
+
+    WeakReference<Component> lastReceiver;
+    HashMap<String, WeakReference<Component>> receiverChildren;
+
+    bool sendHotkeyCommand(Hotkey key,
+        WeakReference<Component> root,
+        WeakReference<Component> target);
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(HotkeyScheme)
+};
