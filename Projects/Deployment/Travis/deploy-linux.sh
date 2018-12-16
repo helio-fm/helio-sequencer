11244d46f61b4b87fcbc4f13d52b20ee3b4d8052
Peter Rudenko <peter.rudenko@gmail.com>
2018-12-16 15:11:38 +0400

    Simplify AppImage build and update its icon

--- Projects/Deployment/Travis/deploy-linux.sh
+++ Projects/Deployment/Travis/deploy-linux.sh
@@ -3,28 +3,24 @@
 set -e
 # TODO check for $TRAVIS_BRANCH and process develop and master branches differently
 
-# For develop build, just create a tarball and upload
+# Create tarball
 cd ${TRAVIS_BUILD_DIR}/Projects/LinuxMakefile/build
 tar --transform 's/.*/\L&/' -czf /tmp/deploy.tar.gz ./Helio
 scp -C /tmp/deploy.tar.gz deploy@helio.fm:/opt/musehackers/files/ci/helio-dev-64-bit.tar.gz
 
+# Create AppImage
 cd ${TRAVIS_BUILD_DIR}
-sed -i 's,/opt/helio_workstation/,,g' ./Projects/Deployment/Linux/Helio-*-amd64/usr/share/applications/Helio.desktop
-sed -i 's,helio_workstation,helio,g' ./Projects/Deployment/Linux/Helio-*-amd64/usr/share/applications/Helio.desktop
+mkdir -p ./Projects/Deployment/Linux/AppImage/x64/usr/bin
+cp ./Projects/LinuxMakefile/build/Helio ./Projects/Deployment/Linux/AppImage/x64/usr/bin/helio
+chmod +x ./Projects/Deployment/Linux/AppImage/x64/usr/bin/helio
 
-cp Projects/LinuxMakefile/build/Helio ./Projects/Deployment/Linux/Helio-*-amd64/usr/bin/helio
-chmod +x ./Projects/Deployment/Linux/Helio-*-amd64/usr/bin/helio
-
-# # Copy icon to here
-cp ./Projects/Deployment/Linux/Helio*-amd64/usr/share/{icons/hicolor/256x256/apps/helio-workstation.png,applications}
-
-unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+unset QTDIR; unset QT_PLUGIN_PATH; unset LD_LIBRARY_PATH
 
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 
 ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
-./squashfs-root/AppRun ./Projects/Deployment/Linux/Helio-*-amd64/usr/share/applications/Helio.desktop -appimage
+./squashfs-root/AppRun ./Projects/Deployment/Linux/AppImage/x64/usr/share/applications/Helio.desktop -appimage
 
 mv Helio*.AppImage helio-dev-64-bit.AppImage
 scp -C helio-dev-64-bit.AppImage deploy@helio.fm:/opt/musehackers/files/ci/helio-dev-64-bit.AppImage
