96de6fd40f548e3a9b7e1658c3045450339789ea
Peter Rudenko <peter.rudenko@gmail.com>
2019-03-31 15:14:36 +0400

    Run iOS deployments only on tagged commits

--- Projects/Deployment/Travis/deploy-ios.sh
+++ Projects/Deployment/Travis/deploy-ios.sh
@@ -6,13 +6,9 @@ if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
     exit 0
 fi
 
-# Assumed to be run either on a development branch or a tagged master branch
-if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
-    APPNAME="helio-dev"
-elif [[ ${TRAVIS_TAG} != "" ]]; then
-    APPNAME="helio-${TRAVIS_TAG}"
-else
-    echo "Skipping deployment: will only run either on a tagged commit, or a develop branch commit"
+# Assumed to be run on a tagged commit
+if [[ ${TRAVIS_TAG} == "" ]]; then
+    echo "Skipping deployment: will only run on a tagged commit"
     exit 0
 fi
 
