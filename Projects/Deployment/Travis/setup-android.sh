4089a19861fdd6013ac5dcc93c3164469be30608
Peter Rudenko <peter.rudenko@gmail.com>
2019-09-01 11:55:20 +0400

    Fix Android CI build not to fail on PRs

--- Projects/Deployment/Travis/setup-android.sh
+++ Projects/Deployment/Travis/setup-android.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Decrypt the secrets
-openssl aes-256-cbc -K $encrypted_cf8a6ca4972b_key -iv $encrypted_cf8a6ca4972b_iv -in ${TRAVIS_BUILD_DIR}/Projects/Deployment/Travis/helio.keystore.enc -out ${HOME}/helio.keystore -d
+set -e
+
+if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
+
+    # For PR builds, copy a public debug keystore
+    cp ${TRAVIS_BUILD_DIR}/Projects/Deployment/Travis/helio.debug.jks ${HOME}/helio.keystore
+
+else
+
+    # Decrypt the release keystore
+    openssl aes-256-cbc -K $encrypted_cf8a6ca4972b_key -iv $encrypted_cf8a6ca4972b_iv -in ${TRAVIS_BUILD_DIR}/Projects/Deployment/Travis/helio.keystore.enc -out ${HOME}/helio.keystore -d
+
+fi
 
 # NDK is not included in Travis initial setup:
 echo y | sdkmanager 'ndk-bundle'
