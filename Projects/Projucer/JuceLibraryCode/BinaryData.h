4e4f31640fcaa03b41c20a1fba9ab20ed9d4da2b
Peter Rudenko <peter.rudenko@gmail.com>
2020-07-05 19:52:19 +0400

    Update built-in translations

--- Projects/Projucer/JuceLibraryCode/BinaryData.h
+++ Projects/Projucer/JuceLibraryCode/BinaryData.h
@@ -336,7 +336,7 @@ namespace BinaryData
     const int            scales_jsonSize = 3513;
 
     extern const char*   translations_json;
-    const int            translations_jsonSize = 143715;
+    const int            translations_jsonSize = 138296;
 
     // Number of elements in the namedResourceList and originalFileNames arrays.
     const int namedResourceListSize = 110;
