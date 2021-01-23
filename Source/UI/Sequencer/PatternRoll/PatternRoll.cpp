helio-fm/helio-sequencer

commit d04001a280e064efbfa81b5d17d09eb8b3c07332

Highlight all instances of selected clips in the pattern roll
When looking at a single row in the pattern roll, it was hard to tell
where are the "instances" and where are other tracks of the same name
or color; this change is supposed to give at least some visual cue

Source/UI/Sequencer/PatternRoll/PatternRoll.cpp
@@ -53,15 +53,6 @@
 #include "ColourIDs.h"
 #include "Config.h"
 
-//void dumpDebugInfo(Array<MidiTrack *> tracks)
-//{
-//    DBG("--- tracks:");
-//    for (int i = 0; i < tracks.size(); i++)
-//    {
-//        DBG(tracks[i]->getTrackName());
-//    }
-//}
-
 struct StringComparator final
 {
     static int compareElements(const String &first, const String &second)
@@ -245,33 +236,6 @@ void PatternRoll::updateRollSize()
 }
 
 //===----------------------------------------------------------------------===//
-// Ghost clips
-//===----------------------------------------------------------------------===//
-
-void PatternRoll::showGhostClipFor(ClipComponent *targetClipComponent)
-{
-    auto *component = new DummyClipComponent(*this, targetClipComponent->getClip());
-    component->setEnabled(false);
-    component->setGhostMode();
-
-    this->addAndMakeVisible(component);
-    this->ghostClips.add(component);
-
-    this->batchRepaintList.add(component);
-    this->triggerAsyncUpdate();
-}
-
-void PatternRoll::hideAllGhostClips()
-{
-    for (int i = 0; i < this->ghostClips.size(); ++i)
-    {
-        this->fader.fadeOut(this->ghostClips.getUnchecked(i), Globals::UI::fadeOutShort);
-    }
-
-    this->ghostClips.clear();
-}
-
-//===----------------------------------------------------------------------===//
 // Clip management
 //===----------------------------------------------------------------------===//
 
@@ -401,7 +365,6 @@ void PatternRoll::onChangeTrackProperties(MidiTrack *const track)
 void PatternRoll::onRemoveTrack(MidiTrack *const track)
 {
     this->selection.deselectAll();
-    this->hideAllGhostClips();
 
     this->tracks.removeAllInstancesOf(track);
     this->reloadRowsGrouping();
@@ -461,8 +424,6 @@ void PatternRoll::onRemoveClip(const Clip &clip)
 {
     if (const auto deletedComponent = this->clipComponents[clip].get())
     {
-        this->hideAllGhostClips();
-
         this->fader.fadeOut(deletedComponent, Globals::UI::fadeOutLong);
         this->selection.deselect(deletedComponent);
         this->clipComponents.erase(clip);
@@ -516,6 +477,32 @@ void PatternRoll::findLassoItemsInArea(Array<SelectableComponent *> &itemsFound,
         {
             jassert(!itemsFound.contains(component));
             itemsFound.add(component);
+        }
+    }
+}
+
+void PatternRoll::updateHighlightedInstances()
+{
+    for (const auto *track : this->tracks)
+    {
+        bool hasSelectedClip = false;
+        const auto *pattern = track->getPattern();
+
+        // find out if at least one instance in a pattern is selected
+        for (const auto *clip : pattern->getClips())
+        {
+            const auto component = this->clipComponents.find(*clip);
+            if (component.value()->isSelected())
+            {
+                hasSelectedClip = true;
+                break;
+            }
+        }
+
+        for (const auto *clip : pattern->getClips())
+        {
+            const auto component = this->clipComponents.find(*clip);
+            component.value()->setHighlightedAsInstance(hasSelectedClip);
         }
     }
 }

Source/UI/Sequencer/MidiEventComponent.cpp
@@ -26,6 +26,7 @@ MidiEventComponent::MidiEventComponent(HybridRoll &editor, bool isGhost) noexcep
 {
     this->flags.isActive = true;
     this->flags.isSelected = false;
+    this->flags.isInstanceOfSelected = false;
     this->flags.isRecordingTarget = false;
     this->flags.isGhost = isGhost;
 

Source/UI/Sequencer/MidiEventComponent.h
@@ -61,10 +61,11 @@ protected:
 
     struct MidiEventComponentFlags final
     {
-        bool isSelected : 1;
-        bool isActive : 1;
-        bool isGhost : 1;
-        bool isRecordingTarget : 1;
+        bool isSelected : 1;            // both clips and notes can be displayed as selected
+        bool isInstanceOfSelected : 1;  // used to highlights all "instances" of selected clips
+        bool isActive : 1;              // whether a note belongs to the selected track or not
+        bool isGhost : 1;               // indicates helper notes which are used for visual cue
+        bool isRecordingTarget : 1;     // indicates that a clip is used as a target to MIDI recording
     };
 
     union

Source/UI/Sequencer/PatternRoll/ClipComponents/ClipComponent.cpp
@@ -66,7 +66,8 @@ void ClipComponent::updateColours()
     this->headBrightColour = Colours::white
         .interpolatedWith(this->getClip().getTrackColour(), 0.55f)
         .withAlpha(this->flags.isGhost ? 0.2f : 0.7f)
-        .brighter(this->flags.isSelected ? 0.25f : 0.f);
+        .brighter(this->flags.isSelected ? 0.25f : 0.f)
+        .darker(this->flags.isInstanceOfSelected ? 0.25f : 0.f);
 
     this->headDarkColour = this->headBrightColour.withAlpha(0.25f);
 
@@ -126,7 +127,6 @@ void ClipComponent::mouseDown(const MouseEvent &e)
 
         forEachSelectedClip(selection, selectedClip)
         {
-            //if (selection.shouldDisplayGhostClips()) { selectedClip->getRoll().showGhostClipFor(selectedClip); }
             selectedClip->startDragging();
         }
     }
@@ -175,12 +175,12 @@ void ClipComponent::mouseDrag(const MouseEvent &e)
             {
                 PatternOperations::duplicateSelection(this->getRoll().getLassoSelection(), false);
 
-                this->getRoll().hideAllGhostClips();
-
                 forEachSelectedClip(selection, clipComponent)
                 {
                     clipComponent->toFront(false);
                 }
+
+                this->getRoll().updateHighlightedInstances();
             }
 
             for (const auto &s : selection.getGroupedSelections())
@@ -282,6 +282,17 @@ void ClipComponent::paint(Graphics &g)
         g.drawText(this->clip.getPattern()->getTrack()->getTrackName(),
             textBounds, Justification::bottomLeft, false);
     }
+    else if (this->flags.isInstanceOfSelected)
+    {
+        constexpr float dashLength = 4.f;
+        constexpr float halfDash = dashLength / 2.f;
+        for (float i = halfDash; i < w - dashLength; i += dashLength * 2.f)
+        {
+            g.fillRect(i + 1.f, 1.f, dashLength, 1.f);
+            g.fillRect(i, 2.f, dashLength, 1.f);
+            //g.fillRect(i, 3.f, dashLength, 1.f);
+        }
+    }
     
     if (this->clip.getKey() != 0)
     {
@@ -304,6 +315,37 @@ void ClipComponent::paint(Graphics &g)
 
     // speedup: set colour to be used by all child components, so they don't have to 
     g.setColour(this->clip.isMuted() ? this->headDarkColour : this->eventColour);
+}
+
+//===----------------------------------------------------------------------===//
+// SelectableComponent
+//===----------------------------------------------------------------------===//
+
+void ClipComponent::setSelected(bool selected)
+{
+    const bool stateIsChanging = selected != this->isSelected();
+    MidiEventComponent::setSelected(selected);
+    if (stateIsChanging)
+    {
+        // this is not super effective (nested loops etc),
+        // but way simpler code and there won't be too much clips
+        // in the project anyway - tens, maybe hundreds:
+        this->getRoll().updateHighlightedInstances();
+    }
+}
+
+void ClipComponent::setHighlightedAsInstance(bool isHighlighted)
+{
+    // if already selected, don't highlight it as an instance:
+    const bool isInstanceOfSelected =
+        !this->flags.isSelected && isHighlighted;
+
+    if (this->flags.isInstanceOfSelected != isInstanceOfSelected)
+    {
+        this->flags.isInstanceOfSelected = isInstanceOfSelected;
+        this->updateColours();
+        this->roll.triggerBatchRepaintFor(this);
+    }
 }
 
 //===----------------------------------------------------------------------===//

Source/UI/Sequencer/PatternRoll/ClipComponents/ClipComponent.h
@@ -66,6 +66,14 @@ public:
     void mouseUp(const MouseEvent &e) override;
     void paint(Graphics& g) override;
 
+    //===------------------------------------------------------------------===//
+    // SelectableComponent
+    //===------------------------------------------------------------------===//
+
+    void setSelected(bool selected) override;
+
+    void setHighlightedAsInstance(bool isHighlighted);
+
     static int compareElements(ClipComponent *first, ClipComponent *second);
 
 protected:
@@ -83,8 +91,6 @@ protected:
     void startTuning();
     Clip continueTuning(const MouseEvent &e) const noexcept;
     Clip continueTuningLinear(float delta) const noexcept;
-    Clip continueTuningMultiplied(float factor) const noexcept;
-    Clip continueTuningSine(float factor, float midline, float phase) const noexcept;
     void endTuning();
 
     friend class PatternRoll;

Source/UI/Sequencer/PatternRoll/PatternRoll.h
@@ -39,13 +39,6 @@ public:
     int getNumRows() const noexcept;
 
     //===------------------------------------------------------------------===//
-    // Ghost clips
-    //===------------------------------------------------------------------===//
-    
-    void showGhostClipFor(ClipComponent *targetClipComponent);
-    void hideAllGhostClips();
-
-    //===------------------------------------------------------------------===//
     // Note management
     //===------------------------------------------------------------------===//
 
@@ -85,11 +78,12 @@ public:
     void findLassoItemsInArea(Array<SelectableComponent *> &itemsFound,
         const Rectangle<int> &rectangle) override;
 
+    void updateHighlightedInstances();
+
     //===------------------------------------------------------------------===//
     // SmoothZoomListener
     //===------------------------------------------------------------------===//
 
-    // TODO: should we zoom rows?
     float getZoomFactorY() const noexcept override;
 
     //===------------------------------------------------------------------===//
@@ -158,12 +152,12 @@ private:
 
 private:
     
-    // Only needed for grouping:
+    // needed for grouping:
     Array<String> rows;
-    Array<const MidiTrack *> tracks;
     void reloadRowsGrouping();
 
-    OwnedArray<ClipComponent> ghostClips;
+    // for grouping and highlighting:
+    Array<const MidiTrack *> tracks;
 
     OwnedArray<ChangeListener> selectionListeners;
 

commit 40f717d083431d2b61a1f1b5425a7ea8e45b3651

Fix some unsafe lookup usage
Possible crash introduced in d04001a280e064efbfa81b5d17d09eb8b3c07332

Source/UI/Sequencer/PatternRoll/PatternRoll.cpp
@@ -493,7 +493,8 @@ void PatternRoll::updateHighlightedInstances()
         for (const auto *clip : pattern->getClips())
         {
             const auto component = this->clipComponents.find(*clip);
-            if (component.value()->isSelected())
+            if (component != this->clipComponents.end() &&
+                component.value()->isSelected())
             {
                 hasSelectedClip = true;
                 break;
@@ -503,7 +504,10 @@ void PatternRoll::updateHighlightedInstances()
         for (const auto *clip : pattern->getClips())
         {
             const auto component = this->clipComponents.find(*clip);
-            component.value()->setHighlightedAsInstance(hasSelectedClip);
+            if (component != this->clipComponents.end())
+            {
+                component.value()->setHighlightedAsInstance(hasSelectedClip);
+            }
         }
     }
 }