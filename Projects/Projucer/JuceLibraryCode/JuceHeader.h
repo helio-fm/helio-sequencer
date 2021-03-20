2478430730bb7a1dc7943c10ac93c24f53bcfe0a
Peter Rudenko <peter.rudenko@gmail.com>
2021-03-20 15:04:30 +0400

    Bump version

--- Projects/Projucer/JuceLibraryCode/JuceHeader.h
+++ Projects/Projucer/JuceLibraryCode/JuceHeader.h
@@ -48,7 +48,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Helio";
     const char* const  companyName    = "Peter Rudenko";
-    const char* const  versionString  = "3.4";
-    const int          versionNumber  = 0x30400;
+    const char* const  versionString  = "3.5";
+    const int          versionNumber  = 0x30500;
 }
 #endif
