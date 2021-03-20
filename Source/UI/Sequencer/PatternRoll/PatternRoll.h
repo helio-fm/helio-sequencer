93d44a4e400d0516ccfa10b50ceafd6f9d56ea63
Peter Rudenko <peter.rudenko@gmail.com>
2021-03-20 11:08:10 +0400

    Fix a glitch: pattern roll not updating with time signature changes
    For whatever reason I overlooked that pattern roll overrides some
    project listener methods of the base class, which repaints when any
    time signature is added, removed or updated

--- Source/UI/Sequencer/PatternRoll/PatternRoll.h
+++ Source/UI/Sequencer/PatternRoll/PatternRoll.h
@@ -52,11 +52,6 @@ public:
     // ProjectListener
     //===------------------------------------------------------------------===//
 
-    void onAddMidiEvent(const MidiEvent &event) override {}
-    void onChangeMidiEvent(const MidiEvent &e1, const MidiEvent &e2) override {}
-    void onRemoveMidiEvent(const MidiEvent &event) override {}
-    void onPostRemoveMidiEvent(MidiSequence *const layer) override {}
-
     void onAddClip(const Clip &clip) override;
     void onChangeClip(const Clip &oldClip, const Clip &newClip) override;
     void onRemoveClip(const Clip &clip) override;
