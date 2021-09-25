helio-fm/helio-sequencer

commit 439cca7e7f73e9406e3269635e8b7951407e86dd

Minor refactoring

Source/Core/Audio/Transport/MidiRecorder.cpp
@@ -49,13 +49,14 @@ MidiRecorder::~MidiRecorder()
     this->getTransport().removeTransportListener(this);
 }
 
-void MidiRecorder::setTargetScope(WeakReference<MidiTrack> track,
-    const Clip *clip, const String &instrumentId)
+void MidiRecorder::setTargetScope(const Clip *clip, const String &instrumentId)
 {
     if (instrumentId.isNotEmpty())
     {
         this->lastValidInstrumentId = instrumentId;
     }
+
+    auto *track = clip->getPattern()->getTrack();
 
     if (this->activeTrack != track || this->activeClip != clip)
     {

Source/Core/App.cpp
@@ -482,14 +482,13 @@ void App::setTitleBarComponent(WeakReference<Component> component)
 
 void App::initialise(const String &commandLine)
 {
-    this->runMode = App::NORMAL;
     if (commandLine.isNotEmpty() &&
         DocumentHelpers::getTempSlot(commandLine).existsAsFile())
     {
-        this->runMode = App::PLUGIN_CHECK;
+        this->runMode = RunMode::PluginCheck;
     }
 
-    if (this->runMode == App::NORMAL)
+    if (this->runMode == RunMode::Normal)
     {
         DBG("Helio v" + App::getAppReadableVersion());
 
@@ -563,7 +562,7 @@ void App::initialise(const String &commandLine)
 
 #endif
     }
-    else if (this->runMode == App::PLUGIN_CHECK)
+    else if (this->runMode == RunMode::PluginCheck)
     {
         this->checkPlugin(commandLine);
         this->quit();
@@ -572,7 +571,7 @@ void App::initialise(const String &commandLine)
 
 void App::shutdown()
 {
-    if (this->runMode == App::NORMAL)
+    if (this->runMode == RunMode::Normal)
     {
         this->config->getUiFlags()->removeListener(this);
 
@@ -606,7 +605,7 @@ void App::shutdown()
 
 const String App::getApplicationName()
 {
-    if (this->runMode == App::PLUGIN_CHECK)
+    if (this->runMode == RunMode::PluginCheck)
     {
         return "Helio Plugin Check";
     }

Source/Core/App.h
@@ -44,10 +44,9 @@ private:
     JUCE_PREVENT_HEAP_ALLOCATION
 };
 
-class App final :
-    public JUCEApplication,
-    private UserInterfaceFlags::Listener,
-    private AsyncUpdater
+class App final : public JUCEApplication,
+                  private UserInterfaceFlags::Listener,
+                  private AsyncUpdater
 {
 public:
 
@@ -64,7 +63,7 @@ public:
     static bool isRunningOnPhone();
     static bool isRunningOnTablet();
     static bool isRunningOnDesktop();
-    
+
     static String getDeviceId();
     static String getAppReadableVersion();
     static String getHumanReadableDate(const Time &date);
@@ -123,13 +122,13 @@ private:
 
     void checkPlugin(const String &markerFile);
 
-    enum RunMode
+    enum class RunMode
     {
-        NORMAL,
-        PLUGIN_CHECK
+        Normal,
+        PluginCheck
     };
 
-    App::RunMode runMode;
-    
+    RunMode runMode = RunMode::Normal;
+
     void handleAsyncUpdate() override;
 };

Source/Core/Audio/Transport/MidiRecorder.h
@@ -37,8 +37,7 @@ public:
 
     ~MidiRecorder() override;
 
-    void setTargetScope(WeakReference<MidiTrack> track,
-        const Clip *clip, const String &instrumentId);
+    void setTargetScope(const Clip *clip, const String &instrumentId);
 
 private:
 

Source/Core/CommandPalette/CommandPaletteMoveNotesMenu.cpp
@@ -77,7 +77,7 @@ const CommandPaletteActionsProvider::Actions &CommandPaletteMoveNotesMenu::getAc
             withCallback([this, targetTrack, &closestClip](TextEditor &)
         {
             SequencerOperations::moveSelection(this->roll.getLassoSelection(), closestClip, true);
-            this->project.setEditableScope(targetTrack, closestClip, false);
+            this->project.setEditableScope(closestClip, false);
             return true;
         }));
     }

Source/Core/CommandPalette/CommandPaletteTimelineEvents.cpp
@@ -175,7 +175,7 @@ const CommandPaletteActionsProvider::Actions &CommandPaletteTimelineEvents::getA
 
                 const auto action = [this, pianoTrackNode, clip](TextEditor &ed)
                 {
-                    this->project.setEditableScope(pianoTrackNode, *clip, true);
+                    this->project.setEditableScope(*clip, true);
                     return true;
                 };
 

Source/Core/Midi/Sequences/Events/Note.cpp
@@ -95,7 +95,6 @@ Note Note::copyWithNewId(WeakReference<MidiSequence> owner) const noexcept
 Note Note::withKey(Key newKey) const noexcept
 {
     Note other(*this);
-    // fixme other.key = jlimit(0, Globals::maxNoteKey, newKey);
     other.key = jmax(0, newKey);
     return other;
 }
@@ -110,7 +109,6 @@ Note Note::withBeat(float newBeat) const noexcept
 Note Note::withKeyBeat(Key newKey, float newBeat) const noexcept
 {
     Note other(*this);
-    // fixme other.key = jlimit(0, Globals::maxNoteKey, newKey);
     other.key = jmax(0, newKey);
     other.beat = roundBeat(newBeat);
     return other;
@@ -119,7 +117,6 @@ Note Note::withKeyBeat(Key newKey, float newBeat) const noexcept
 Note Note::withKeyLength(Key newKey, float newLength) const noexcept
 {
     Note other(*this);
-    // fixme other.key = jlimit(0, Globals::maxNoteKey, newKey);
     other.key = jmax(0, newKey);
     other.length = jmax(Globals::minNoteLength, roundBeat(newLength));
     return other;
@@ -135,7 +132,6 @@ Note Note::withDeltaBeat(float deltaPosition) const noexcept
 Note Note::withDeltaKey(Key deltaKey) const noexcept
 {
     Note other(*this);
-    // fixme other.key = jlimit(0, Globals::maxNoteKey, other.key + deltaKey);
     other.key = jmax(0, other.key + deltaKey);
     return other;
 }

Source/Core/Tree/ProjectNode.cpp
@@ -245,11 +245,12 @@ void ProjectNode::showPatternEditor(WeakReference<TreeNode> source)
     App::Layout().showPage(this->sequencerLayout.get(), source);
 }
 
-void ProjectNode::setMidiRecordingTarget(MidiTrack *const track, const Clip *clip)
+void ProjectNode::setMidiRecordingTarget(const Clip *clip)
 {
     String instrumentId;
-    if (track != nullptr && clip != nullptr)
+    if (clip != nullptr)
     {
+        auto *track = clip->getPattern()->getTrack();
         auto &audioCore = App::Workspace().getAudioCore();
 
         instrumentId = track->getTrackInstrumentId();
@@ -268,14 +269,12 @@ void ProjectNode::setMidiRecordingTarget(MidiTrack *const track, const Clip *cli
         audioCore.setActiveMidiPlayer(instrumentId, false);
     }
 
-    this->midiRecorder->setTargetScope(track, clip, instrumentId);
+    this->midiRecorder->setTargetScope(clip, instrumentId);
 }
 
-void ProjectNode::setEditableScope(MidiTrack *const activeTrack,
-    const Clip &activeClip, bool shouldFocusToArea)
+void ProjectNode::setEditableScope(const Clip &activeClip, bool shouldFocusToArea)
 {
-    // fixme: remove activeTrack from parameters, this is ridiculous
-    jassert(activeClip.getPattern()->getTrack() == activeTrack);
+    auto *activeTrack = activeClip.getPattern()->getTrack();
 
     if (auto *item = dynamic_cast<PianoTrackNode *>(activeTrack))
     {
@@ -289,7 +288,7 @@ void ProjectNode::setEditableScope(MidiTrack *const activeTrack,
         this->changeListeners.call(&ProjectListener::onChangeViewEditableScope,
             activeTrack, activeClip, shouldFocusToArea);
 
-        this->setMidiRecordingTarget(activeTrack, &activeClip);
+        this->setMidiRecordingTarget(&activeClip);
     }
 }
 

commit e08b6f3ced30a73a4674971371a03ad8dd12d57f

Fix a regression
Fixes a crash introduced in 439cca7e7f73e9406e3269635e8b7951407e86dd

Source/Core/Audio/Transport/MidiRecorder.cpp
@@ -56,7 +56,7 @@ void MidiRecorder::setTargetScope(const Clip *clip, const String &instrumentId)
         this->lastValidInstrumentId = instrumentId;
     }
 
-    auto *track = clip->getPattern()->getTrack();
+    auto *track = clip == nullptr ? nullptr : clip->getPattern()->getTrack();
 
     if (this->activeTrack != track || this->activeClip != clip)
     {