aab45a6e1ebadfe716f64bccd5308404953bf919
ryanpmcguire <ryanpmcguire01@gmail.com>
2022-11-23 16:26:45 -0400

    Add mouseOffset when adding note for greater click accuracy

--- Source/UI/Sequencer/PianoRoll/PianoRoll.cpp
+++ Source/UI/Sequencer/PianoRoll/PianoRoll.cpp
@@ -1454,7 +1454,8 @@ void PianoRoll::insertNewNoteAt(const MouseEvent &e)
 {
     int key = 0;
     float beat = 0.f;
-    this->getRowsColsByMousePosition(e.x, e.y, key, beat);
+    int xOffset = 5;    //pretend the cursor is 5px to the right of where it actually is. massively boosts click accuracy when placing notes! - RPM
+    this->getRowsColsByMousePosition(e.x + xOffset, e.y, key, beat);
 
     auto *activeSequence = static_cast<PianoSequence *>(this->activeTrack->getSequence());
     activeSequence->checkpoint();
