0d8a3dd19a19b3989ab0335bee6258678691fc1b
Peter Rudenko <peter.rudenko@gmail.com>
2023-12-12 06:40:17 +0400

    Bump project version

--- Projects/Deployment/Windows/setup.iss
+++ Projects/Deployment/Windows/setup.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "Helio"
-#define MyAppVersion "3.11"
+#define MyAppVersion "3.12"
 #define MyAppPublisher "Peter Rudenko"
 #define MyAppURL "https://helio.fm"
 #define MyAppExeName "Helio.exe"
--- Projects/Projucer/JuceLibraryCode/JuceHeader.h
+++ Projects/Projucer/JuceLibraryCode/JuceHeader.h
@@ -48,7 +48,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Helio";
     const char* const  companyName    = "Peter Rudenko";
-    const char* const  versionString  = "3.11";
-    const int          versionNumber  = 0x30b00;
+    const char* const  versionString  = "3.12";
+    const int          versionNumber  = 0x30c00;
 }
 #endif
