c308bf6887d7c72f13362a4db7fd7f2efb4efabf
Peter Rudenko <peter.rudenko@gmail.com>
2026-04-05 11:15:36 +0400

    Cleanup: rework the legacy colour scheme

--- Projects/Projucer/JuceLibraryCode/BinaryData.h
+++ Projects/Projucer/JuceLibraryCode/BinaryData.h
@@ -363,7 +363,7 @@ namespace BinaryData
     const int            chords_jsonSize = 768;
 
     extern const char*   colourSchemes_json;
-    const int            colourSchemes_jsonSize = 6223;
+    const int            colourSchemes_jsonSize = 6182;
 
     extern const char*   hotkeySchemes_json;
     const int            hotkeySchemes_jsonSize = 26137;
